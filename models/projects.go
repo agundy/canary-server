8d64a6a5e58ca2d95b52ab4349deac2f635b8c76
Charles Goepfert <ForceofaDragon4@gmail.com>
2016-10-20 14:29:26 -0400

    Actually added projects model

---
+++ models/projects.go
@@ -0,0 +1,30 @@
+package models
+
+import (
+	"fmt"
+
+	"github.com/jinzhu/gorm"
+	_ "github.com/jinzhu/gorm/dialects/postgres"
+	"math/rand"
+)
+
+type Project struct {
+	gorm.Model
+	Name string `json:"name"`
+	UserID uint `gorm:"index"`
+	Token string `gorm:"index"`
+}
+
+func (p *Project) GenerateToken() {
+	
+
+}
+// Something I found on StackOverflow... should look into actual RNG in Go 
+// func randSeq(n int) string {
+// 	var letters = []rune("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ")
+//     b := make([]rune, n)
+//     for i := range b {
+//         b[i] = letters[rand.Intn(len(letters))]
+//     }
+//     return string(b)
+// }
