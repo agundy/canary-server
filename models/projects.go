09ab8e35e23e11d5fdf05f80d0625260d69bab12
Charles Goepfert <ForceofaDragon4@gmail.com>
2016-10-20 17:23:02 -0400

    added CreateProjects funciton

--- models/projects.go
+++ models/projects.go
@@ -2,10 +2,14 @@ package models
 
 import (
 	// "fmt"
+	"errors"
+	"log"
 
 	"github.com/jinzhu/gorm"
 	_ "github.com/jinzhu/gorm/dialects/postgres"
 	// "math/rand"
+
+	"github.com/agundy/canary-server/database"
 )
 
 type Project struct {
@@ -28,3 +32,28 @@ func (p *Project) GenerateToken() {
 //     }
 //     return string(b)
 // }
+
+// CreateProject takes a project object and sets the proper fields
+func CreateProject(p *Project) (newProject *Project, err error) {
+	var queryProject Project
+
+	// Check if project name is blank
+	if p.Name == "" {
+		log.Println("Project name can't be blank")
+		return nil, errors.New("Project name can't be blank")
+	}
+
+	// Check if a project with given name is already in database
+	database.DB.Where(&Project{Name: p.Name}).First(&queryProject)
+	if queryProject.Name != "" {
+		log.Println("Found project: ", queryProject)
+		return nil, errors.New("Project already exists")
+	}
+
+	newProject = &Project{Name: p.Name, UserID: p.UserID}
+	newProject.GenerateToken()
+
+	database.DB.Create(&newProject)
+
+	return newProject, err
+}
